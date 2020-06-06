09ba6ebb912c048e88674ee0822c56fa2cb8b122
Maciej Budzyński <maciej.budzynski@spoko.pl>
2020-06-06 09:32:24 +0200

    Feature/swagger (#12)
    * Poczatkowa implementacja swaggera
    
    Signed-off-by: Maciej Budzyński <maciej.budzynski@spoko.pl>
    
    * Rozbudowana implementacja swaggera z nowym profilem aplikacji swagger
    
    Signed-off-by: Maciej Budzyński <maciej.budzynski@spoko.pl>

--- src/main/java/pl/kul/cinemix/profiles/AppProfiles.java
+++ src/main/java/pl/kul/cinemix/profiles/AppProfiles.java
@@ -8,6 +8,8 @@ public class AppProfiles {
 
     public static final String H2_CONSOLE = "h2console";
 
+    public static final String SWAGGER = "swagger";
+
     public static final String NOT = "!";
 
 }
