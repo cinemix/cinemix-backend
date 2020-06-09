d69021b7f8cf47ac1261a58f6ca18f5f2d0d5a3a
Maciej Budzyński <maciej.budzynski@spoko.pl>
2020-06-09 12:09:32 +0200

    Podbicie wersji Springa i optymalizacja importów
    Signed-off-by: Maciej Budzyński <maciej.budzynski@spoko.pl>

--- src/main/java/pl/kul/cinemix/models/Movie.java
+++ src/main/java/pl/kul/cinemix/models/Movie.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,8 +13,6 @@ import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 
 @Getter
